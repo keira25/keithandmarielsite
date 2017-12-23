6254c0dabc299362ef333c9829507745531378cd
keira25 <keira1425@gmail.com>
2017-12-24 01:04:32 +0800

    Time to sleep

--- keithandmariel/urls.py
+++ keithandmariel/urls.py
@@ -17,10 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    
+
     url(r'^admin/', admin.site.urls),
     url(r'^', include('kmpersonal.urls')),
-    url(r'^photos/', include('kmphotos.urls')),
-    url(r'^blog/', include('kmblog.urls')),   
     
 ]
