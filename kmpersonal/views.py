4c722193951059174b5bb881249b0987aaf2f66f
keira25 <keira1425@gmail.com>
2017-12-24 00:57:58 +0800

    Setup kmpersonal app

--- kmpersonal/views.py
+++ kmpersonal/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'personal/home.html')
+
+def story(request):
+    return render(request, 'personal/story.html')
