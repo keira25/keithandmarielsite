8669992158f433ae7d4f52761130f730bb5ea6f2
keira25 <keira1425@gmail.com>
2017-12-24 00:43:44 +0800

    Added startapp kmpersonal, kmphotos, kmblog

---
+++ kmpersonal/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KmpersonalConfig(AppConfig):
+    name = 'kmpersonal'
---
+++ kmphotos/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KmphotosConfig(AppConfig):
+    name = 'kmphotos'
